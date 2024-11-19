1fd98ca162ddfd268077d17ec5634fff30eeb0b2
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-11-19 18:13:55 -0400

    feat: connect to database using '.env'

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id CHARACTER(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    serial_number INTEGER NOT NULL,
+    birth_date DATE NOT NULL,
+    birth_certificate blob,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL 
+);
