7914c754603300b48b83b8702bc62e56d1e0023d
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-11-19 15:14:28 -0400

    feat: init project

---
+++ src/test/java/br/mattsousa/crud_template/CrudTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mattsousa.crud_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
