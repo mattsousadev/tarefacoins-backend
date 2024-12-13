9994e36aa0a5e25123d1629245a4e4a1b1c1918f
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-12-13 11:16:43 -0400

    fix: handle http body request error

--- src/main/java/br/mattsousa/beans/ApiExceptionHandler.java
+++ src/main/java/br/mattsousa/beans/ApiExceptionHandler.java
@@ -1,7 +1,10 @@
 package br.mattsousa.beans;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -43,4 +46,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+            ErrorResponse body = new ErrorResponse();
+            body.setSuccess(status.is2xxSuccessful());
+            body.setCode(status.value());
+            String error = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+            body.setMessage(error);
+            return ResponseEntity.status(status).body(body);
+    }
+
+    
 }
