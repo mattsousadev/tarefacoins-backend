d325437b25d777130226ccc22c85980a8354d977
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-12-13 09:01:30 -0400

    fix: remove template code

--- src/main/java/br/mattsousa/data/repository/FilesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.mattsousa.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.mattsousa.data.models.FilesModel;
-
-@Repository
-public interface FilesRepository extends JpaRepository<FilesModel, String> { }
